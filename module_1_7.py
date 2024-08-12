61bbd7cccd46a13b17e3312ab787d4930ab27b20
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-08-12 21:49:57 +0300

    Commit

--- module_1_7.py
+++ module_1_7.py
@@ -1,7 +1,15 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron', })  # Упорядочить по алфавиту, и на выходе присваиваю список, а не множество
 
-students = sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron', })  # Упорядочить по алфавиту, и на выходе список
+average_grades = dict(zip(students, grades))    # Склеиваем два списка в словарь
+
+print(grades)
+print(students)
+
+for i in grades:
+    print(sum(i)/len(i))
 
 
-print((grades))
-print((students))
+for j in average_grades.values():
+    print(sum(j)/len(j))
+
