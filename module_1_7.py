1c9a42e9cbb94cb1ae5d3e486951e4cbe64260fa
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-08-13 16:48:56 +0300

    Commit

--- module_1_7.py
+++ module_1_7.py
@@ -2,17 +2,17 @@
 
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron', })  # Упорядочить по алфавиту, и на выходе присваиваю список, а не множество
-#average_grades = dict(zip(students, grades))    # Склеиваем два списка в словарь, хотя было бы рациональнее сначала получать среднее значение, а потом склеивать...
+#average_grades = dict(zip(students, grades))    # Склеиваем два списка в словарь
+ave_grades = []
+
+for a in grades:
+    ave_grades.append(sum(a)/len(a))
 
 
-print('Оценки: ', grades)
-print('Студенты: ',students)
-#
-# for j in average_grades:
-#     print(j)
-#     # print(sum(j)/len(j))
-
-# print('Результат: ', average_grades)
+print('Средние оценки: ', ave_grades)
+print('Студенты: ', students)
+result = dict(zip(students, ave_grades))
+print(result)
 
 
 
